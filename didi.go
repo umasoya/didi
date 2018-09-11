886d0f8c64e20122b7dbe8c214aab81e72d16cf4
umasoya <basilisk.0331@icloud.com>
2018-09-11 13:54:55 +0900

    Check if /etc/os-release exists

--- didi.go
+++ didi.go
@@ -7,11 +7,36 @@ import (
 	"github.com/urfave/cli"
 )
 
+type Identity struct {
+	Name             string
+	Version          string
+	ID               string
+	IDLike           string
+	VersionCodename  string
+	VersionID        string
+	PrettyName       string
+	AnsiColor        string
+	CpeName          string
+	HomeURL          string
+	SupportURL       string
+	BugReportURL     string
+	PrivatePolicyURL string
+	BuildID          string
+	Variant          string
+	VariantID        string
+}
+
 func main() {
 	app := cli.NewApp()
 	app.Name = "didi"
 	app.Usage = "Destinguish distribution"
 	app.Version = "0.9.0"
+
+	// Check if /etc/os-release exists
+	app.Before = func(c *cli.Context) error {
+		_, err := os.Stat("/etc/os-release")
+		return err
+	}
 
 	err := app.Run(os.Args)
 	if err != nil {
